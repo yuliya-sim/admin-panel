7758618c9b7b9228a844a66d87c47b2ebcd8394c
Yuliya Simonchyk <y.simonchyk@softteco.com>
2023-12-05 09:42:01 +0300

    fix:key map

--- src/dashboard/LastComments.tsx
+++ src/dashboard/LastComments.tsx
@@ -1,21 +1,21 @@
 import CardHeader from "@material-ui/core/CardHeader";
-import {CardActions, CardContent, Grid, Button} from "@mui/material";
+import { CardActions, CardContent, Grid } from "@mui/material";
 import Card from "@material-ui/core/Card";
 import data from '../data';
-import {useMemo} from "react";
-import {Link} from "@material-ui/core";
+import { useMemo } from "react";
+import { Link } from "@material-ui/core";
 
 export const LastComments = () => {
     const resentComments = useMemo(() => {
         return data.comments.slice(-3)
     }, [data])
 
-    return <Card style={{marginBottom: 20}}>
-        <CardHeader title="Last comments"/>
+    return <Card style={{ marginBottom: 20 }}>
+        <CardHeader title="Last comments" />
         <Grid container={true} gap={'10px'} >
             {resentComments.map(el => {
-                return <Card>
-                    <CardHeader title={el.author.name}/>
+                return <Card key={crypto.randomUUID()}>
+                    <CardHeader title={el.author.name} />
                     <CardContent>{el.body}</CardContent>
                     <CardActions><Link>Go to post</Link></CardActions>
                 </Card>
