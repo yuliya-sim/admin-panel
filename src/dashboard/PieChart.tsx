cecf76f19374790ecd303a0ad319ea4ccbe1ab06
Yuliya Simonchyk <y.simonchyk@softteco.com>
2023-12-05 10:39:34 +0300

    add:fetch data pie chart

--- src/dashboard/PieChart.tsx
+++ src/dashboard/PieChart.tsx
@@ -1,50 +1,52 @@
-import {Pie, PieChart as PieChartComponent, Cell, Tooltip} from "recharts";
-import {useMemo} from "react";
-import data from '../data';
+import { Pie, PieChart as PieChartComponent, Cell, Tooltip } from "recharts";
 
-const data1 = [
-    {name: 'Group A', value: 400},
-    {name: 'Group B', value: 300},
-    {name: 'Group C', value: 300},
-    {name: 'Group D', value: 200},
-];
+import {
+    useGetList,
+} from 'react-admin';
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
 const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({cx, cy, midAngle, innerRadius, outerRadius, percent, index}) => {
+
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius }) => {
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-    return (
-        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-            {`${(percent * 100).toFixed(0)}%`}
-        </text>
-    );
-};
-
+}
 export const PieChart = () => {
 
-    const parsedData = useMemo(() => {
-        const res = data.posts.reduce((acc, el) => {
-            if (acc[el.category]) {
-                acc[el.category] += 1;
-            } else {
-                acc[el.category] = 1;
-            }
-            return acc
-        }, {})
+    let sortedData
+    let combinedData
+    const fetchedData = useGetList('posts');
+    if (fetchedData) {
+        sortedData = fetchedData.data?.map(item => ({ value: item.views, name: item.category }))
+        if (sortedData) {
+            combinedData = Object.values(
+                sortedData.reduce((acc, curr) => {
+                    const { name, value } = curr;
 
-        return Object.entries(res).map(([name, value]) => ({name, value}))
-    }, [data])
+                    if (!acc[name]) {
+                        acc[name] = { name, value };
+                    } else {
+                        acc[name].value += value;
+                    }
+                    return acc;
+                }, {})
+            );
+        }
+
+    }
+
+    if (!combinedData) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <>
-            <h4 style={{textAlign: 'center'}}>Categories</h4>
+            <h4 style={{ textAlign: 'center' }}>Categories</h4>
             <PieChartComponent width={400} height={250}>
                 <Pie
-                    data={parsedData}
+                    data={combinedData}
                     cx="50%"
                     cy="50%"
                     labelLine={false}
@@ -53,12 +55,13 @@ export const PieChart = () => {
                     fill="#8884d8"
                     dataKey="value"
                 >
-                    {parsedData.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
+                    {combinedData.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                     ))}
                 </Pie>
-                <Tooltip/>
+                <Tooltip />
             </PieChartComponent>
         </>
     )
 }
+
